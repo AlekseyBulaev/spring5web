a7d7c91180eda17d0b150d76ffa77abd9e456f21
bulaev <bulaev@phoenixit.ru>
2019-06-19 13:03:40 +0300

    Initial commit

---
+++ src/main/java/study/abulaev/spring5web/Spring5webApplication.java
@@ -0,0 +1,13 @@
+package study.abulaev.spring5web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/abulaev/spring5web/Spring5webApplicationTests.java
@@ -0,0 +1,16 @@
+package study.abulaev.spring5web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5webApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
